88a6b8837e02d35f0c0c8f550f5cea89eb07b27e
nomuonji <63096515+nomuonji@users.noreply.github.com>
2026-09-26 00:02:16 +0900

    巨大な資格サイドバーを自動折りたたみ化

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -119,6 +119,12 @@ const config: Config = {
   ],
 
   themeConfig: {
+    docs: {
+      sidebar: {
+        hideable: true,
+        autoCollapseCategories: true,
+      },
+    },
     // Replace with your project's social card
     image: 'img/og-default.png',
     metadata: [
